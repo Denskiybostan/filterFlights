d1d856aef3740d9e178afb26eba407af43d5840a
denis <deniz246@mail.ru>
2024-09-22 22:22:45 +0300

    исправление, попытка № 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,73 +1,173 @@
 package org.example;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         List<Flight> flights = FlightBuilder.createFlights();
-        System.out.println("Изначальные полеты\n"+ flights);
+        System.out.println("Изначальные полеты\n" + flights);
 
-        System.out.println("\nБез вылетов до текущей даты\n"+ flightsFilterDepartureTime(flights));
-        System.out.println("\nБез прилетов до вылета\n"+ flightsFilterArrivalBeforeDeparture(flights));
-        System.out.println("\nБез полетов с ожиданием больше двух часов\n"+ flightsFilterWaitingMoreThenTwoHours(flights));
+        System.out.println("\nБез вылетов до текущей даты\n" + flightsFilterDepartureTime(flights));
+        System.out.println("\nБез прилетов до вылета\n" + flightsFilterArrivalBeforeDeparture(flights));
+        System.out.println("\nБез полетов с ожиданием больше двух часов\n" + filterFlightsWhereTimeSpentOnGroundMoreThanTwoHours(flights));
+    }
+    public static List<Flight> flightsFilterDepartureTime(List<Flight> initialFlights) {
+        return initialFlights.stream()
+                .filter(flight -> flight.getSegments().stream()
+                        .noneMatch(segment -> segment.getArrivalDate().isBefore(LocalDateTime.now())))
+                .collect(Collectors.toList());
+    }
+    public static List<Flight> flightsFilterArrivalBeforeDeparture(List<Flight> initialFlights) {
+        return initialFlights.stream()
+                .filter(flight -> flight.getSegments().stream()
+                        .noneMatch(segment -> segment.getArrivalDate().isBefore(segment.getDepartureDate())))
+                .collect(Collectors.toList());
     }
 
-    public static List<Flight> flightsFilterDepartureTime (List<Flight> initialFlights) {
-        List<Flight> filteredFlights = new ArrayList<>(); //создание пустого массива с последующей фильтрацией по указанному признаку
-        for(Flight flight : initialFlights){
-            boolean isSegmentIncorrect = false;
-            for (Segment segment : flight.getSegments()){
-                if (segment.getDepartureDate().isBefore(LocalDateTime.now())) {
-                    isSegmentIncorrect = true;
-                }
-            }
-            if (!isSegmentIncorrect) {
-                filteredFlights.add(flight);
-            }
+    public static List<Flight> filterFlightsWhereTimeSpentOnGroundMoreThanTwoHours(List<Flight> setFlights) {
+        return setFlights.stream()
+                .filter(flight -> methodFilter(flight).toHours() <= 2)
+                .collect(Collectors.toList());
+    }
 
+
+    private static Duration methodFilter(Flight flight) {
+        List<Segment> segments = flight.getSegments();
+        Duration timeWait = Duration.ZERO;
+        for (int i = 1; i < segments.size(); i++) {
+            LocalDateTime timeDeparture = segments.get(i).getDepartureDate();
+            LocalDateTime timeArrival = segments.get(i - 1).getArrivalDate();
+            Duration groundTime = Duration.between(timeArrival, timeDeparture);
+            timeWait = timeWait.plus(groundTime);
         }
-        System.out.println(filteredFlights);
-        return filteredFlights;
+        return timeWait;
+    }
+    }
+//    public static List<Flight> filterFlightsWhereTimeSpentOnGroundMoreThanTwoHoursTests(List<Flight> setFlights) {
+//
+//        return setFlights.stream()
+//                .forEach(Flight::getSegments)
+//
+//                .filter(flight -> {
+//
+//                    Duration totalGroundTime = Duration.ZERO;
+//                    List<Segment> segments = flight.getSegments();
+//                    for (int i = 1; i < segments.size(); i++) {
+//                        LocalDateTime timeDeparture = segments.get(i).getDepartureDate();
+//                        LocalDateTime timeArrival = segments.get(i - 1).getArrivalDate();
+//                        Duration groundTime = Duration.between(timeArrival, timeDeparture);
+//                        totalGroundTime = totalGroundTime.plus(groundTime);
+//                    }
+//                    return totalGroundTime.toHours() < 2;
+//                })
+//                .collect(Collectors.toList());
+//    }
 
-    }
-    private static List<Flight> flightsFilterArrivalBeforeDeparture (List<Flight> initialFlights) {
-        List<Flight> filteredFlights = new ArrayList<>();
-        for(Flight flight : initialFlights){
-            boolean isSegmentIncorrect = false;
-            for (Segment segment : flight.getSegments()){
-                if (segment.getArrivalDate().isBefore(segment.getDepartureDate())) {
-                    isSegmentIncorrect = true;
-                }
-            }
-            if (!isSegmentIncorrect) {
-                filteredFlights.add(flight);
-            }
+//public static List<Flight> filterFlightsWhereTimeSpentOnGroundMoreThanTwoHours(List<Flight> setFlights) {
+//    List<Flight> filteredFlights = new ArrayList<>();
+//    Duration totalGroundTime = Duration.ZERO;
+//    for (Flight flight : setFlights) {
+//        List<Segment> segments = flight.getSegments();
+//        for (int i = 1; i < segments.size(); i++) {
+//            LocalDateTime timeDeparture = segments.get(i).getDepartureDate();
+//            LocalDateTime timeArrival = segments.get(i - 1).getArrivalDate();
+//
+//            Duration groundTime = Duration.between(timeArrival, timeDeparture);
+//            totalGroundTime = totalGroundTime.plus(groundTime);
+//        }
+//        if (totalGroundTime.toHours() > 2) {
+//            filteredFlights.add(flight);
+//        }
+//        totalGroundTime = Duration.ZERO;
+//    }
+//    return filteredFlights;
+//}
 
-        }
-        System.out.println(filteredFlights);
-        return filteredFlights;
-    }
-    private static List<Flight> flightsFilterWaitingMoreThenTwoHours (List<Flight> initialFlights) {
-        List<Flight> filteredFlights = new ArrayList<>();
-        for(Flight flight : initialFlights){
-            boolean isSegmentIncorrect = false;
-            Segment previousSegment=null;
-            for (Segment segment : flight.getSegments()){
-                if (previousSegment != null) {
-                    if (segment.getDepartureDate().isAfter(previousSegment.getArrivalDate().plusHours(2))) {
-                        isSegmentIncorrect = true;
-                    }
-                }
-                previousSegment = segment;
-            }
-            if (!isSegmentIncorrect) {
-                filteredFlights.add(flight);
-            }
-        }
-        System.out.println(filteredFlights);
-        return filteredFlights;
-    }
-}
+//    private static List<Flight> flightsFilterArrivalBeforeDeparture(List<Flight> initialFlights) {
+//        List<Flight> filteredFlights = new ArrayList<>();
+//        for (Flight flight : initialFlights) {
+//            boolean isSegmentIncorrect = false;
+//            for (Segment segment : flight.getSegments()) {
+//                if (segment.getArrivalDate().isBefore(segment.getDepartureDate())) {
+//                    isSegmentIncorrect = true;
+//                }
+//            }
+//            if (!isSegmentIncorrect) {
+//                filteredFlights.add(flight);
+//            }
+//        }
+//        System.out.println(filteredFlights);
+//        return filteredFlights;
+//    }
+
+
+//
+//    private static List<Flight> flightsFilterWaitingMoreThenTwoHours(List<Flight> initialFlights) {
+//        List<Flight> filteredFlights = new ArrayList<>();
+//        for (Flight flight : initialFlights) {
+//            boolean isSegmentIncorrect = false;
+//            Segment previousSegment = null;
+//            LocalDateTime waitingTime = null;
+//            for (Segment segment : flight.getSegments()) {
+//                waitingTime = segment.getDepartureDate();
+//                    if (previousSegment != null && waitingTime != null) {
+//
+//                        if (segment.getDepartureDate().isBefore(waitingTime)) {
+//                            isSegmentIncorrect = true;
+//                            break;
+//                        }
+//                    }
+//                previousSegment = segment;
+//            }
+//            if (!isSegmentIncorrect) {
+//                filteredFlights.add(flight);
+//            }
+//            System.out.println(filteredFlights);
+//        }
+//        return filteredFlights;
+//    }
+//}
+
+
+
+
+//    private static List<Flight> flightsFilterWaitingMoreThenTwoHours1 (List<Flight> initialFlights) {
+//        List<Flight> filteredFlights = new ArrayList<>();
+//        for(Flight flight : initialFlights){
+//            boolean isSegmentIncorrect = false;
+//            Segment previousSegment=null;
+//            for (Segment segment : flight.getSegments()){
+//                if (previousSegment != null) {
+//                    if (segment.getDepartureDate().isAfter(previousSegment.getArrivalDate().plusHours(2))) {
+//                        isSegmentIncorrect = true;
+//                    }
+//                }
+//                previousSegment = segment;
+//            }
+//            if (!isSegmentIncorrect) {
+//                filteredFlights.add(flight);
+//            }
+//        }
+//        System.out.println(filteredFlights);
+//        return filteredFlights;
+//    }
+//public static List<Flight> flightsFilterDepartureTime(List<Flight> initialFlights) {
+//        List<Flight> filteredFlights = new ArrayList<>(); //создание пустого массива с последующей фильтрацией по указанному признаку
+//        for (Flight flight : initialFlights) {
+//            boolean isSegmentIncorrect = false;
+//            for (Segment segment : flight.getSegments()) {
+//                if (segment.getDepartureDate().isBefore(LocalDateTime.now())) {
+//                    isSegmentIncorrect = true;
+//                }
+//            }
+//            if (!isSegmentIncorrect) {
+//                filteredFlights.add(flight);
+//            }
+//        }
+//        System.out.println(filteredFlights);
+//        return filteredFlights;
+//    }
+
--- src/main/java/org/example/TestClasses.java
+++ src/main/java/org/example/TestClasses.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -15,28 +16,28 @@ class FlightBuilder {
     static List<Flight> createFlights() {
         LocalDateTime threeDaysFromNow = LocalDateTime.now().plusDays(3);
         return Arrays.asList(
-            //A normal flight with two hour duration
-            createFlight(threeDaysFromNow, threeDaysFromNow.plusHours(2)),
-            //A normal multi segment flight
-            createFlight(threeDaysFromNow, threeDaysFromNow.plusHours(2),
-                threeDaysFromNow.plusHours(3), threeDaysFromNow.plusHours(5)),
-            //A flight departing in the past
-            createFlight(threeDaysFromNow.minusDays(6), threeDaysFromNow),
-            //A flight that departs before it arrives
-            createFlight(threeDaysFromNow, threeDaysFromNow.minusHours(6)),
-            //A flight with more than two hours ground time
-            createFlight(threeDaysFromNow, threeDaysFromNow.plusHours(2),
-                threeDaysFromNow.plusHours(5), threeDaysFromNow.plusHours(6)),
-            //Another flight with more than two hours ground time
-            createFlight(threeDaysFromNow, threeDaysFromNow.plusHours(2),
-                threeDaysFromNow.plusHours(3), threeDaysFromNow.plusHours(4),
-                threeDaysFromNow.plusHours(6), threeDaysFromNow.plusHours(7)));
+                //A normal flight with two hour duration
+                createFlight(threeDaysFromNow, threeDaysFromNow.plusHours(2)),
+                //A normal multi segment flight
+                createFlight(threeDaysFromNow, threeDaysFromNow.plusHours(2),
+                        threeDaysFromNow.plusHours(3), threeDaysFromNow.plusHours(5)),
+                //A flight departing in the past
+                createFlight(threeDaysFromNow.minusDays(6), threeDaysFromNow),
+                //A flight that departs before it arrives
+                createFlight(threeDaysFromNow, threeDaysFromNow.minusHours(6)),
+                //A flight with more than two hours ground time
+                createFlight(threeDaysFromNow, threeDaysFromNow.plusHours(2),
+                        threeDaysFromNow.plusHours(5), threeDaysFromNow.plusHours(6)),
+                //Another flight with more than two hours ground time
+                createFlight(threeDaysFromNow, threeDaysFromNow.plusHours(2),
+                        threeDaysFromNow.plusHours(3), threeDaysFromNow.plusHours(4),
+                        threeDaysFromNow.plusHours(6), threeDaysFromNow.plusHours(7)));
     }
 
     private static Flight createFlight(final LocalDateTime... dates) {
         if ((dates.length % 2) != 0) {
             throw new IllegalArgumentException(
-                "you must pass an even number of dates");
+                    "you must pass an even number of dates");
         }
         List<Segment> segments = new ArrayList<>(dates.length / 2);
         for (int i = 0; i < (dates.length - 1); i += 2) {
@@ -50,6 +51,7 @@ class FlightBuilder {
  * Bean that represents a flight.
  */
 class Flight {
+
     private final List<Segment> segments;
 
     Flight(final List<Segment> segs) {
@@ -67,10 +69,12 @@ class Flight {
     }
 }
 
+
 /**
  * Bean that represents a flight segment.
  */
 class Segment {
+
     private final LocalDateTime departureDate;
 
     private final LocalDateTime arrivalDate;
@@ -95,5 +99,6 @@ class Segment {
         return '[' + departureDate.format(fmt) + '|' + arrivalDate.format(fmt)
             + ']';
     }
+
 }
 
